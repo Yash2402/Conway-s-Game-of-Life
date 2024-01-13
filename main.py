7d71aa4377062881fa30a2d1ee206b0da6ea2c1c
Yash2402 <mrsmeenakumaris@gmail.com>
2024-01-13 13:26:36 +0530

    modified:   main.py

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import random
 import numpy as np
 import pygame
 
-
 pygame.init()
 
+# UPDATE THE GRID ACCORDING TO THE CONWAY'S GAME OF LIFE RULES.
 def update(currgen, gridx, gridy):
     next = np.zeros((gridx, gridy))
     for i in range(gridx):
@@ -20,73 +20,89 @@ def update(currgen, gridx, gridy):
     return next
                 
 
-def countNeighbours(currgen, x, y):
+# COUNT THE NUMBER OF ALIVE CELLS AROUND A PARTICULAR CELL.
+def countNeighbours(currgen, i, j):
     sum = 0
-    for i in range(-1, 2):
-        for j in range(-1, 2):
-            a = (x + i + gridx) % gridx
-            b = (y + j + gridy) % gridy
+    for a in range(-1, 2):
+        for b in range(-1, 2):
+            a = (i + a + gridx) % gridx
+            b = (j + b + gridy) % gridy
             sum += currgen[a][b]
-    sum -= currgen[x][y]
+    sum -= currgen[i][j]
     return sum
 
+# PYGAME WINDOW 
+screen = pygame.display.set_mode((0, 0),pygame.RESIZABLE) # CREATE PYGAME WINDOW.
+pygame.display.set_caption("Conway's Game Of Life") # SET THE WINDOWS NAME TO "Conway's Game Of Life".
 
-screen = pygame.display.set_mode((0, 0),pygame.RESIZABLE)
-pygame.display.set_caption("Conway's Game Of Life")
-width = 16
-gridx = screen.get_width()//width
-gridy = screen.get_height()//width
+# VARIABLES
+width = 16 # CHANGES THE WIDTH OF THE CELLS.
+upd = False # IF THIS IS TRUE ONLY THEN THE GIRD WILL UPDATE.
+gridx = screen.get_width()//width # NUMBER OF COLUMNS.
+gridy = screen.get_height()//width # NUMBER OF ROWS.
 
-run = True
-currgen = np.zeros((gridx, gridy))
+currgen = np.zeros((gridx, gridy)) # NUMPY 2 DIMENSIONAL ARRAY STORING CELLS STATE.
+
+# INITIAL STATE (RANDOM 0s and 1s)
 for i, j in np.ndindex(currgen.shape):
     currgen[i][j] = random.randint(0, 1)
-upd = False
-tick = pygame.time.Clock()
 
+
+tick = pygame.time.Clock()
+run = True
+# GAME LOOP
 while run:
     pygame.display.flip()
-    tick.tick(30)
-    screen.fill((24, 24, 24))
+    tick.tick(30) # SET THE FRAME RATE OF THE PYGAME WINDOW.
+    screen.fill((24, 24, 24)) 
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+        # ADD ALIVE CELL
         if pygame.mouse.get_pressed()[0]:
             mousepos = pygame.mouse.get_pos()
             i = mousepos[0]//width
             j = mousepos[1]//width
             currgen[i][j] = 1
+
+        # ADD DEAD CELL
         if pygame.mouse.get_pressed()[2]:
             mousepos = pygame.mouse.get_pos()
             i = mousepos[0]//width
             j = mousepos[1]//width
             currgen[i][j] = 0
         if event.type == pygame.KEYDOWN:
+            # STOP UPDATING
             if event.key == pygame.K_SPACE:
                 upd = not upd
+            # CLEAR THE GRID/MAKE ALL CELLS DEAD
             if event.key == pygame.K_r:
                 upd = False
                 currgen = np.zeros((gridx, gridy))
 
+    # DRAWING PREVIOUS STATE
     for i in range(gridx):
         for j in range(gridy):
             if currgen[i][j]:
-                # color1 = (150, 150, 150)
                 prev_color = (160, 160, 160)
             else:
                 prev_color = (0, 0, 0)
+                
             pygame.draw.rect(screen, prev_color, (i*width + 1, j*width + 1, width-1, width-1), 3)
-    
+
+    # UPDATING
     if upd:        
         currgen = update(currgen, gridx, gridy)
-    
+
+    # DRAWING CURRENT STATE
     for i in range(gridx):
         for j in range(gridy):
             if currgen[i][j]:
                 color = (255, 255, 255)
             else:
                 color = (0, 0, 0)
+
             pygame.draw.rect(screen, color, (i*width + 1, j*width + 1, width-1, width-1), 2)
-    pygame.display.update()
 
-
+    pygame.display.update() # UPDATE THE PYGAME WINDOW
